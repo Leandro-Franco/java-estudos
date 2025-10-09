a963eefb2a9cba34c0c49d97451230388e361bf3
leandro <leandrofigueiredofranco@gmail.com>
2025-10-08 23:27:02 -0300

    subindo repo pro git

---
+++ src/Arrays.java
@@ -0,0 +1,8 @@
+public class Arrays{
+    public static void main(String[] args){
+        float a = 2.1f;
+        int b = 2;
+        boolean isEqual = a == b;
+        System.out.println(isEqual);
+    }
+}
---
+++ src/Bank.java
@@ -0,0 +1,10 @@
+
+public class Bank {
+    public static void main(String[] args) {
+        ContaBancaria conta = new ContaBancaria(100);
+        conta.mostraMensagem();
+        conta.depositar(500);
+        double valorConvertido = conta.converteMoeda(30, 5);
+    }
+}
+
