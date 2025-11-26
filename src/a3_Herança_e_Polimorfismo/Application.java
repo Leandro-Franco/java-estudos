f98e2a404cccb14799799387127841549a2dc7a6
leandro <leandrofigueiredofranco@gmail.com>
2025-11-26 01:33:33 -0300

    polimorfismos

--- src/a3_Herança_e_Polimorfismo/Application.java
+++ src/a3_Herança_e_Polimorfismo/Application.java
@@ -4,5 +4,16 @@ public class Application {
 
   public static void main(String[] args) {
 
+    Warrior player1 = new Warrior();
+    player1.setName("Aragorn");
+    // A linha abaixo funcionará normalmente
+    player1.setWeapon("Espada");
+
+    PlayableCharacter player2 = new Warrior();
+    player2.setName("Aragorn");
+
+    // A linha abaixo dará erro se for descomentada
+    // player2.setWeapon("Espada");
   }
 }
+
--- src/a3_Herança_e_Polimorfismo/Cleric.java
+++ src/a3_Herança_e_Polimorfismo/Cleric.java
@@ -4,18 +4,11 @@ public class Cleric extends PlayableCharacter {
 
   private String magic; // magia
 
-
   public String getMagic() {
-
     return magic;
-
   }
-
 
   public void setMagic(String magic) {
-
     this.magic = magic;
-
   }
-
 }
